7a0449b766020389e9444096949bb8f3d53ca257
Lele Luu <leleluu@gmail.com>
2021-01-08 23:23:28 +0100

    Add model for tea

---
+++ TeaTimerV2/TeaTimerV2/Models/Tea.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Tea {
+    let name: String
+    let brewTime: Int
+
+    init(name: String, brewTime: Int) {
+        self.name = name
+        self.brewTime = brewTime
+    }
+
+}
