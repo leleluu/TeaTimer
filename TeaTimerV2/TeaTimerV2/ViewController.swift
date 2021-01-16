04fa09114e9644ec5425c48986d492dcfa5408e5
Lele Luu <leleluu@gmail.com>
2021-01-16 11:52:15 +0100

    Add dummy tea data

--- TeaTimerV2/TeaTimerV2/ViewController.swift
+++ TeaTimerV2/TeaTimerV2/ViewController.swift
@@ -2,9 +2,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //TODO: Remove dummy data
+    let earlGrey = Tea(name: "Earl Grey", brewTime: 1)
+    let rooibos = Tea(name: "Rooibos", brewTime: 2)
+    let jasmine = Tea(name: "Jasmine", brewTime: 3)
+
+    var teaCollection = [Tea]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        teaCollection.append(earlGrey)
+        teaCollection.append(rooibos)
+        teaCollection.append(jasmine)
     }
 }
 
