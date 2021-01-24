d76a635a93548940fcb56d675d2984ed55567204
Lele Luu <leleluu@gmail.com>
2021-01-24 12:22:23 +0100

    Use auto-generated initialiser for structs

--- TeaTimerV2/TeaTimerV2/Models/Tea.swift
+++ TeaTimerV2/TeaTimerV2/Models/Tea.swift
@@ -3,10 +3,4 @@ import Foundation
 struct Tea {
     let name: String
     let brewTime: Int
-
-    init(name: String, brewTime: Int) {
-        self.name = name
-        self.brewTime = brewTime
-    }
-
 }
