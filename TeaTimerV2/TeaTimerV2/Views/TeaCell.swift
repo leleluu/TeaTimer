97f61bd936903f72ec559fd6369cd18cff7a659b
Lele Luu <leleluu@gmail.com>
2021-02-18 22:13:55 +0100

    Add icon to tea cell

--- TeaTimerV2/TeaTimerV2/Views/TeaCell.swift
+++ TeaTimerV2/TeaTimerV2/Views/TeaCell.swift
@@ -4,7 +4,8 @@ class TeaCell: UICollectionViewCell {
 
     // MARK: Properties
 
-var teaNameLabel = UILabel()
+    var teaNameLabel = UILabel()
+    var teaImageView = UIImageView(image: UIImage(named: "teacup"))
 
     // MARK: Initialiser
     
@@ -21,20 +22,31 @@ var teaNameLabel = UILabel()
     // MARK: Private methods
 
     private func setupViews() {
-        contentView.backgroundColor = .systemYellow
 
-        // TeaNameLabel
+        contentView.backgroundColor = .systemYellow
+        let teaImageViewSizeConstant = contentView.frame.height * 0.6
+
+        // Tea name label
         contentView.addSubview(teaNameLabel)
         teaNameLabel.translatesAutoresizingMaskIntoConstraints = false
         teaNameLabel.textColor = .white
         teaNameLabel.textAlignment = .center
         teaNameLabel.numberOfLines = 2
 
+        // Tea imageview
+        contentView.addSubview(teaImageView)
+        teaImageView.translatesAutoresizingMaskIntoConstraints = false
+        teaImageView.contentMode = .scaleAspectFit
+
         // Constraints
         NSLayoutConstraint.activate([
+            teaImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            teaImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            teaImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            teaImageView.heightAnchor.constraint(equalToConstant: teaImageViewSizeConstant),
+            teaImageView.bottomAnchor.constraint(equalTo: teaNameLabel.topAnchor, constant: -8),
             teaNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
             teaNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
-            teaNameLabel.topAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 8),
             teaNameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
         ])
     }
